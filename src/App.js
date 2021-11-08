71b7d249b445a825103d1eef8ac0736388328b64
Praveen Ramachandran <praveen-thayikkattil@users.noreply.github.com>
2021-11-08 15:11:32 +0530

    Header + Hero HTML

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="App-logo" title="Habit Theory">Habit Theory</h1>
+      <header className="app-header">
+        <h1 className="app-logo" title="Habit Theory">Habit Theory</h1>
+
+        <button className="nav-open">MENU</button>
       </header>
+
+      <section className="home-hero">
+        <blockquote>
+          <p>"We use insights from neuroscience, psychology and behavioural economics to help our clients build productive microhabits."</p>
+        </blockquote>
+      </section>
+
+      <section className="who-we-are">Who we are</section>
+      
+      <section className="who-we-are">Our services</section>
+
+      <section className="who-we-are">Founders</section>
+
+      <section className="who-we-are">What we live by</section>
+
+      <section className="who-we-are">Why choose us</section>
     </div>
   );
 }
